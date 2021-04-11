01a78dfb8de6f0bf48844d03aeece9fd2346e3af
Denem Orhun <denemorhun@gmail.com>
2021-04-11 23:52:53 +0300

    phase 3 complete

--- src/passwordStrengthVerifier.py
+++ src/passwordStrengthVerifier.py
@@ -2,6 +2,7 @@
 # author: Denem Orhun
 
 ''' Changelog (latest on top):
+    
     # Add Phase 3
     # Add test scaffolding to cover Phase 3 and Phase 4
     # Refactor Phase 1 & 2
@@ -35,14 +36,12 @@ Expected behavior: The password is accepted
 
 '''
 
-# refactored code to include a class for standard user and admin user
-# User class that has verify password method
+# User class with verify password method
 class User():
-
     def __init__(self):
         self.CHAR_LIMIT = 8
 
-    def verify_password(self, password):
+    def _verify_password(self, password):
 
         alpha_status = True
         digit_status = True
@@ -64,18 +63,19 @@ class User():
 
         return True
 
-    def _print_helper(self, input):
+    def is_pwd_valid(self, input):
         print("Password is accepted" if input else "Password is not accepted")
 
+# Admin Class extends user and 
 class Admin(User):
 
     def __init__(self):
         self.CHAR_LIMIT = 13
         self.special_chars = ['!', '@', '#', '$', '%', '^', '&', '*']
 
-    def verify_admin_password(self, password):
+    def _verify_admin_password(self, password):
 
-        basic_status = self.verify_password(password)
+        basic_status = self._verify_password(password)
         print("Basic Status", basic_status)
         special_char_status = True
 
@@ -93,25 +93,3 @@ class Admin(User):
         else:
             print('Returning true')
             return True
-
-    
-
-# user1  = User()
-# user1._print_helper(user1.verify_password('111111111111'))
-# user1._print_helper(user1.verify_password('8888dddd'))
-# user1._print_helper(user1.verify_password('        '))
-# user1._print_helper(user1.verify_password('uiurerererare'))
-# user1._print_helper(user1.verify_password('ADERE3232425'))
-# user1._print_helper(user1.verify_password('@#$@#$#@$@$$@$@'))
-# user1._print_helper(user1.verify_password('@#$@$@ADAFDAF23423424'))
-# user1._print_helper(user1.verify_password('111111111111'))
-
-
-admin1 = Admin()
-admin1._print_helper(admin1.verify_admin_password('111'))
-admin1._print_helper(admin1.verify_admin_password('111111111@11111111'))
-admin1._print_helper(admin1.verify_admin_password('@1111dddddddddddd'))
-
-admin1._print_helper(admin1.verify_admin_password('ABDC23423443dadafdad'))
-
-print(admin1.verify_admin_password('ABDC23423443dadafdad'))
